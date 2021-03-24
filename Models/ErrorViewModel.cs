506914c2727a4f5e5876f9addc69f3059ee15278
Joshua Sintos <jsintos15@gmail.com>
2021-03-25 03:35:14 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ingredient_inator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
