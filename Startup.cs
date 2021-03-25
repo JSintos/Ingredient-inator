c572136957657cf25d1484bc1baa35c4c457b202
Joshua Sintos <jsintos15@gmail.com>
2021-03-25 14:39:48 +0800

    Connected the web application to the SQL server

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.EntityFrameworkCore;
+using Ingredient_inator.Data;
+
 namespace Ingredient_inator
 {
     public class Startup
@@ -23,6 +26,9 @@ namespace Ingredient_inator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(option =>
+                option.UseSqlServer(Configuration.GetConnectionString("MyConnection")));
+
             services.AddControllersWithViews();
         }
 
