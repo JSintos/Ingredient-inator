b6d0c27e75657d3bb3787fae18242dfc91f22dcb
Joshua Sintos <jsintos15@gmail.com>
2021-03-26 17:36:27 +0800

    Added Identity to the project

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ingredient_inator.Data;
+using Ingredient_inator.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Ingredient_inator.Areas.Identity.IdentityHostingStartup))]
+namespace Ingredient_inator.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
