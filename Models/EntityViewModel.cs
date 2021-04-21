01b97f78f01076d6c351429f2794428f1c96339a
Josh Lagarejos <joshuagabriel.lagarejos@benilde.edu.ph>
2021-04-21 22:46:22 +0800

    Created profile page and entity view model, user can view all their entities

---
+++ Models/EntityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ingredient_inator.Models
+{
+    public class EntityViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Recipe> Recipes { get; set; }
+        public List<Review> Reviews { get; set; }
+    }
+}
