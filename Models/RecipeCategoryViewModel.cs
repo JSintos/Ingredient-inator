ac880b076c88a422434d8bcc6242c3d55cd27945
Joshua Sintos <jsintos15@gmail.com>
2021-04-22 16:07:54 +0800

    Front-end changes to validation messages in login and register, revamped the profile - manage entities feature

---
+++ Models/RecipeCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ingredient_inator.Models
+{
+    public class RecipeCategoryViewModel
+    {
+        public List<Recipe> Recipes { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
